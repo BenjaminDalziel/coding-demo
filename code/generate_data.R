a65cb11bfb1cfe15e464469ae69259dd1b97e079
Benjamin Dalziel <benjamin.dalziel@oregonstate.edu>
2024-01-29 10:31:07 -0800

    Generate population data

--- code/generate_data.R
+++ code/generate_data.R
@@ -1,5 +1,30 @@
 # Generate a time series of population abundances
 # by sampling from a normal distribution
 
+# Clear the workspace and close all graphics
 rm(list = ls())
 graphics.off()
+
+
+# Parameters
+mean_abundance <- 1000
+noise_level <- 3
+ntimepoints <- 150
+
+
+# Sample population sizes
+pop_size <- rnorm(n = ntimepoints,
+                  mean = mean_abundance,
+                  sd = noise_level)
+
+
+# Diagnostic plot
+par(cex = 3)
+plot(pop_size,
+     xlab = "Time",
+     ylab = "Population size")
+
+
+# Save data
+filename <- "data/popdata.Rdata"
+save(pop_size, file = filename)
